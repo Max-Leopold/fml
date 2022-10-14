3f9f6a4d3454a8439cc3347ec29cf19dffd8ad2c
Max Leopold <max.leopold2111@gmail.com>
2022-10-14 23:31:29 +0200

    Read factorio server config

---
+++ internal/factorio/server_config.go
@@ -0,0 +1,28 @@
+package factorio
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type ServerConfig struct {
+	Token    string `json:"token"`
+	Username string `json:"username"`
+}
+
+func GetServerConfig(path string) ServerConfig {
+	return readConfig(path)
+}
+
+func readConfig(path string) ServerConfig {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config ServerConfig
+	json.Unmarshal([]byte(file), &config)
+
+	return config
+}
