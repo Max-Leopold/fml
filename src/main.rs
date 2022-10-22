b12557a67afc3b1e8445641dbe774f0cb2b36d52
Max Leopold <max.leopold2111@gmail.com>
2022-10-22 19:04:35 +0200

    Restore terminal when panicking

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 mod factorio;
 mod fml;
 
+use std::{io, panic};
+
 use anyhow::Result;
+use crossterm::event::DisableMouseCapture;
+use crossterm::{execute, cursor};
+use crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
 use fml::app::FML;
 use log::error;
+
+pub fn panic_restore_terminal() {
+    disable_raw_mode().unwrap();
+    let mut terminal = io::stdout();
+    execute!(
+        terminal,
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+        cursor::Show
+    )
+    .unwrap();
+}
 
 fn main() -> Result<()> {
     better_panic::install();
@@ -15,6 +32,11 @@ fn main() -> Result<()> {
         .enable_all()
         .build()?
         .block_on(async {
+            panic::set_hook(Box::new(|panic_info| {
+                panic_restore_terminal();
+                better_panic::Settings::auto().create_panic_handler()(panic_info);
+            }));
+
             FML::new()
                 .with_mods_config(mods_config_path)
                 .with_server_config(server_config_path)
